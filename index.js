0e3dd6b62e4fb8768761e22bb2399d5249cb40af
jainudit012 <jainudit.012@gmail.com>
2019-01-29 13:11:30 +0530

    reformatted

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express()
+const route=require('./routes/route')
 
-router.get('/',async(req,res)=>{
-    res.send('OK')
+router.use('/api',route)
+
+const server = router.listen(3000,()=>{
+    console.log('Listening on port 3000...')
+    
 })
-
-router.listen(3000)
-console.log('Listening on port 3000...')
 
 module.exports= router
