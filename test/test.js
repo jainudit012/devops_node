bc499edee7645d04ad4da91ec090738a614b5f35
jainudit012 <jainudit.012@gmail.com>
2019-01-29 15:25:07 +0530

    test changed

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ const request = require('supertest')
 const router = require('../index.js')
 
 describe('GET /', function() {
-    it('respond with OK', function(done) {
-        request(router).get('/api').expect('OK', done)
+    it('respond with Hello World 2', function(done) {
+        request(router).get('/api').expect('Hello World 2', done)
     })
-    
+
 })
