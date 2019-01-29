b1fded8db10aaded7fa8dabef90ba414c9464858
jainudit012 <jainudit.012@gmail.com>
2019-01-29 16:23:18 +0530

    changed

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router=express.Router()
 
 router.get('/',(req,res)=>{
-    res.send('Hello World 2')
+    res.send('Udit Jain is awesome!!!')
 })
 
 module.exports=router
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 const router = require('../index')
 
 describe('GET /', function() {
-    it('respond with Hello World 2', function(done) {
-        request(router).get('/api').expect('Hello World 2', done)
+    it('respond with Udit Jain is awesome!!!', function(done) {
+        request(router).get('/api').expect('Udit Jain is awesome!!!', done)
     })
 })
